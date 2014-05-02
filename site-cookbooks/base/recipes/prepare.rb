84b691240817f1b70c7ad2ecf25eba47edd0f4e7
morizyun <merii.ken@gmail.com>
2014-05-02 10:23:58 +0900

    add setting of swap memory

--- site-cookbooks/base/recipes/prepare.rb
+++ site-cookbooks/base/recipes/prepare.rb
@@ -23,3 +23,16 @@ bash 'set JST timezone' do
     EOC
   end
 end
+
+bash 'add swap memory' do
+  user 'root'
+  cwd '/var'
+  code <<-EOC
+    mkdir /var/swap
+    dd if=/dev/zero of=/var/swap/swap0 bs=1M count=2048
+    chmod 600 /var/swap/swap
+    mkswap /var/swap/swap0
+    swapon /var/swap/swap0
+    echo '/var/swap/swap0 swap swap defaults 0 0' >> /etc/fstab
+  EOC
+end
